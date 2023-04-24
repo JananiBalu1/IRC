a522c5648524b6a2c5ba8c054195aede0f36ea65
JananiBalu1 <131333280+JananiBalu1@users.noreply.github.com>
2023-04-24 18:07:14 +0530

    day2

--- Day2/src/main/java/com/example/day2/Day2Application.java
+++ Day2/src/main/java/com/example/day2/Day2Application.java
@@ -11,7 +11,7 @@ public class Day2Application {
 
 	public static void main(String[] args) throws JsonProcessingException {
 		SpringApplication.run(Day2Application.class, args);
-		Person obj=new Person(1,"Janani",19);
+		Person obj=new Person(1,"Jananii",19);
 		ObjectMapper obj1=new ObjectMapper();
 		String str=obj1.writeValueAsString(obj);
 		System.out.println(str);
